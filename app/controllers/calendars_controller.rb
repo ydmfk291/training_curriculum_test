ff486a2d670ded2951517cae81169364468ebb70
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-02-22 19:30:51 +0900

    Revert "Merge branch 'second_training' o
    This reverts commit

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_week
+    getWeek
     @plan = Plan.new
   end
 
@@ -13,11 +13,12 @@ class CalendarsController < ApplicationController
   end
 
   private
+
   def plan_params
     params.require(:plan).permit(:date, :plan)
   end
 
-  def get_week
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
