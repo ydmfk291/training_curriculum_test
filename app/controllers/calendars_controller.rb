93665029ae125b24a14b3dcd0c6dc408809e4a5c
ydmfk291 <78467544+ydmfk291@users.noreply.github.com>
2021-02-24 10:25:58 +0900

    Revert "Revert "Revert "Merge pull request #8 from ydmfk291/second_training"""
    This reverts commit f88bb10a47c251f1b33e718eb6105b82695434d1.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -14,7 +14,7 @@ class CalendarsController < ApplicationController
 
   private
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -33,7 +33,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: wdays[(@todays_date+x).wday] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
   end
